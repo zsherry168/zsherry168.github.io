7e14740b7ff0a3376cea30fbba9b9053caf83188
Sherry Zhang <zsherry168@gmail.com>
2023-04-24 02:15:26 -0400

    Made some minor adjustments to resume. Changed email back to .edu email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer () {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16" style={{marginRight: 7}}>
                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                     </svg>
-                    sherrybzhang@gmail.com
+                    sbz5250@psu.edu
                 </p>
                 <p>
                     <a href="https://www.linkedin.com/in/zsherry168/" target="_blank" rel="noreferrer" style={{marginRight: 7}}>
@@ -28,7 +28,7 @@ function Footer () {
                     LinkedIn
                     </a>
                 </p>
-                <p>Last Updated: April 21, 2023</p>
+                <p>Last Updated: April 24, 2023</p>
                 <p class="react">Created with React</p>
             </div>
         </div>
