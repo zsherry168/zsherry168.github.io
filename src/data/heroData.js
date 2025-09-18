d5b54754bb831a5727b2348456c34a5effa511e7
Sherry Zhang <zsherry168@gmail.com>
2025-09-18 11:35:03 -0400

    chore (hero data): slight change in wording for blurb

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -1,7 +1,7 @@
 const heroData = {
   name: "Sherry Zhang",
   title: "Software Engineer",
-  blurb: `Part of Prudential Financial's Global Technology Rotational Program, 
+  blurb: `In Prudential Financial's Global Technology Rotational Program, 
   I build solutions that bring technology, finance, and business together. 
   I'm driven by impact and committed to STEM education and mentorship.`
 };
