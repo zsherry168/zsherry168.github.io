f049bcb0fa160ca0fadbe01376d19f2b50e0c2d8
Sherry Zhang <93848016+zsherry168@users.noreply.github.com>
2025-09-18 14:08:51 -0400

    refactor (home): moved data for hero under its own file (#15)
    * refactor (hero data): transferred hero data to its own js file under data folder
    
    * chore (hero data): slight change in wording for blurb
    
    * refactor (home): refactored links for contact icons. will migrate into components later

---
+++ src/data/heroData.js
@@ -0,0 +1,9 @@
+const heroData = {
+  name: "Sherry Zhang",
+  title: "Software Engineer",
+  blurb: `In Prudential Financial's Global Technology Rotational Program, 
+  I build solutions that bring technology, finance, and business together. 
+  I'm driven by impact and committed to STEM education and mentorship.`
+};
+
+export default heroData;
