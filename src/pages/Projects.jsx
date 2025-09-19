ed9e36656e23b9a8e77fd3a10ec6b15d1df1f0b5
Sherry Zhang <zsherry168@gmail.com>
2025-09-18 21:23:01 -0400

    refactor (projects): replaced old project cards with ContentCard component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,70 +1,22 @@
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import projectCover from "../assets/project_placeholder.jpg";
-
-const projects = [
-  {
-    id: 6,
-    title: "Project 6",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-6",
-  },
-  {
-    id: 5,
-    title: "Project 5",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-5",
-  },
-  {
-    id: 4,
-    title: "Project 4",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-4",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-3",
-  },
-  {
-    id: 2,
-    title: "Project 2",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-2",
-  },
-  {
-    id: 1,
-    title: "Project 1",
-    desc: "Description of project",
-    img: projectCover,
-    link: "/projects/project-1",
-  },
-];
+import { Container, Row, Col } from "react-bootstrap";
+import ContentCard from "../components/ContentCard.jsx";
+import projects from "../data/projects.js";
 
 export default function Projects() {
   return (
     <>
-      <Container className="py-4" style={{ marginTop: "90px" }}>
-        <h1 className="fw-bold display-5 mb-2">Projects</h1>
-
+      <Container className="py-4" style={{ marginTop: "80px", marginBottom: "20px" }}>
+        <h1 className="fw-bold display-5 mb-4">Projects</h1>
         <Row className="g-4">
           {projects.map((p) => (
             <Col key={p.id} xs={12} md={6} lg={4}>
-              <Card className="h-100 shadow-sm">
-                <Card.Img variant="top" src={p.img} alt={`${p.title} cover`} />
-                <Card.Body>
-                  <Card.Title>{p.title}</Card.Title>
-                  <Card.Text className="text-secondary">{p.desc}</Card.Text>
-                  <Button variant="outline-dark" href={p.link}>
-                    View Project
-                  </Button>
-                </Card.Body>
-              </Card>
+              <ContentCard
+                title={p.title}
+                desc={p.desc}
+                img={p.img}
+                link={p.link}
+                buttonText="View Project"
+              />
             </Col>
           ))}
         </Row>
