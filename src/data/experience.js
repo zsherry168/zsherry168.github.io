547bdb20c1d32563e0f1241b72132b39c7c22410
Sherry Zhang <93848016+zsherry168@users.noreply.github.com>
2025-09-19 22:57:14 -0400

    refactor (data): moved experience data into its own js file (#24)
    * feat (logos): added logos to experiences
    
    * style (experience): changed the look of the experience accordions
    
    * refactor (data): moved experience data into its own js file

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,12 +1,117 @@
-export const experience = [
+import pruLogo from "../assets/prudential_logo.png";
+import psuLogo from "../assets/psu_logo.png";
+import awsLogo from "../assets/aws_logo.png";
+import hknLogo from "../assets/hkn_logo.png";
+import gwcLogo from "../assets/gwc_logo.png";
+
+export const professional = [
   {
-    company: "Prudential",
     title: "Software Engineer",
-    dates: "2024–present",
+    org: "Prudential Financial",
+    location: "Newark, NJ",
+    dates: "Jul 2025 – Present",
+    logoSrc: pruLogo,
     bullets: [
-      "Migrated legacy UI to React/Vite",
-      "Improved accessibility & design system adoption"
-    ]
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex sapien vitae pellentesque sem placerat in id.",
+      "Pretium tellus duis convallis tempus leo eu aenean. Uma tempor pulvinar vivamus fringilla lacus nec metus.",
+      "Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    ],
   },
-  // internships / previous roles
+  {
+    title: "Software Engineer Intern",
+    org: "Prudential Financial",
+    location: "Newark, NJ",
+    dates: "Jun 2024 – Aug 2024",
+    logoSrc: pruLogo,
+    bullets: [
+      "Semper vel class aptent taciti sociosqu ad litora. Tempus leo eu aenean sed diam urna tempor.",
+      "Conubia nostra inceptos himenaeos orci varius natoque penatibus. Ut hendrerit semper vel class aptent taciti sociosqu.",
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. "
+    ],
+  },
 ];
+
+export const academic = [
+  {
+    title: "Research Assistant",
+    org: "Penn State College of Engineering",
+    location: "University Park, PA",
+    dates: "Oct 2024 – May 2025",
+    logoSrc: psuLogo,
+    bullets: [
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+      "Quisque faucibus ex sapien vitae pellentesque sem placerat. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+      "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas."
+    ],
+  },
+  {
+    title: "Teaching Assistant",
+    org: "Penn State College of Engineering",
+    location: "University Park, PA",
+    dates: "Jan 2023 – May 2025",
+    logoSrc: psuLogo,
+    bullets: [
+      "In id cursus mi pretium tellus duis convallis. Ut hendrerit semper vel class aptent taciti sociosqu.",
+      "Tempus leo eu aenean sed diam urna tempor. Ad litora torquent per conubia nostra inceptos himenaeos.",
+      "Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis."
+    ],
+  },
+];
+
+export const leadership = [
+  {
+    title: "Mentee",
+    org: "AWS She Builds Mentorship Program",
+    location: "Virtual",
+    dates: "Sep 2025 – Present",
+    logoSrc: awsLogo,
+    bullets: ["In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.",
+      "Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere."
+    ],
+  },
+  {
+    title: "Mentor",
+    org: "Penn State Women in Engineering Program",
+    location: "University Park, PA",
+    dates: "Mar 2023 – May 2024",
+    logoSrc: psuLogo,
+    bullets: ["Iaculis massa nisl malesuada lacinia integer nunc posuere. Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+      "Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    ],
+  },
+  {
+    title: "Study Group Facilitator",
+    org: "Penn State Women in Engineering Program",
+    location: "University Park, PA",
+    dates: "Aug 2023 – Dec 2023",
+    logoSrc: psuLogo,
+    bullets: ["Ut hendrerit semper vel class aptent taciti sociosqu. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      "In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor."
+    ],
+  },
+];
+
+export const volunteering = [
+  {
+    title: "Peer Tutor",
+    org: "IEEE-Eta Kappa Nu Honor Society",
+    location: "University Park, PA",
+    dates: "Sep 2023 – May 2024",
+    logoSrc: hknLogo,
+    bullets: ["Ad litora torquent per conubia nostra inceptos himenaeos. In id cursus mi pretium tellus duis convallis.",
+      "Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    ],
+  },
+  {
+    title: "Instructor",
+    org: "Girls Who Code",
+    location: "University Park, PA",
+    dates: "Jan 2024 – May 2024",
+    logoSrc: gwcLogo,
+    bullets: ["Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex sapien vitae pellentesque sem placerat in id.",
+      "Pretium tellus duis convallis tempus leo eu aenean. Urna tempor pulvinar vivamus fringilla lacus nec metus."
+    ],
+  },
+];
+
+export default { professional, academic, leadership, volunteering };
