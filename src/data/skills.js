9467ff5dfc2e58f1e689fafdceb7f83a9d59f6ca
Sherry Zhang <93848016+zsherry168@users.noreply.github.com>
2025-09-27 16:00:52 -0400

    refactor (skills): skills and logos are stored under its own js file under data (#33)

---
+++ src/data/skills.js
@@ -0,0 +1,146 @@
+import pythonLogo from "../assets/python_logo.png";
+import jsLogo from "../assets/javascript_logo.png";
+import htmlLogo from "../asssets/html_logo.png";
+import cssLogo from "../assets/css_logo.png";
+import cLogo from "../assets/c_logo.png";
+import javaLogo from "../assets/java_logo.png";
+import sqlLogo from "../assets/sql_logo.png";
+
+import flaskLogo from "../assets/flask_logo.png";
+import reactLogo from "../assets/react_logo.png";
+import bootstrapLogo from "../assets/bootstrap_logo.png";
+import pandasLogo from "../assets/panda_logo.png";
+import numpyLogo from "../assets/numpy_logo.png";
+import scikitlearnLogo from "../assets/scikit_learn_logo.png";
+import matplotlibLogo from "../assets/matplotlib_logo.png";
+
+import viteLogo from "../assets/vite_logo.png";
+import gitLogo from "../assets/git_logo.png";
+import githubLogo from "../assets/github_logo.png";
+import bitbucketLogo from "../assets/bitbucket_logo.png";
+import vscodeLogo from "../assets/vscode_logo.png";
+import pycharmLogo from "../assets/pycharm_logo.png";
+
+import genesysLogo from "../assets/genesys_logo.png";
+import salesforceLogo from "../assets/salesforce_logo.png";
+
+import figmaLogo from "../assets/figma_logo.png";
+import adobePhotoshopLogo from "../assets/adobe_photoshop_logo.png";
+import adobeLightroomLogo from "../assets/adobe_lightroom_logo.png";
+
+export const languages = [
+  {
+    label: "Python",
+    logoSrc: pythonLogo,
+  },
+  {
+    label: "JavaScript",
+    logoSrc: jsLogo
+  },
+  {
+    label: "HTML",
+    logoSrc: htmlLogo
+  },
+  {
+    label: "CSS",
+    logoSrc: cssLogo
+  },
+  {
+    label: "C",
+    logoSrc: cLogo
+  },
+  {
+    label: "Java",
+    logoSrc: javaLogo
+  },
+  {
+    label: "SQL",
+    logoSrc: sqlLogo
+  }
+];
+
+export const frameworksLibraries = [
+  {
+    label: "Flask",
+    logoSrc: flaskLogo
+  },
+  {
+    label: "React",
+    logoSrc: reactLogo
+  },
+  {
+    label: "Bootstrap",
+    logoSrc: bootstrapLogo
+  },
+  {
+    label: "Pandas",
+    logoSrc: pandasLogo
+  },
+  {
+    label: "NumPy",
+    logoSrc: numpyLogo
+  },
+  {
+    label: "Scikit-learn",
+    logoSrc: scikitlearnLogo
+  },
+  {
+    label: "Matplotlib",
+    logoSrc: matplotlibLogo
+  }
+];
+
+export const toolsPlatforms = [
+  {
+    label: "Vite",
+    logoSrc: viteLogo
+  },
+  {
+    label: "Git",
+    logoSrc: gitLogo
+  },
+  {
+    label: "GitHub",
+    logoSrc: githubLogo
+  },
+  {
+    label: "Bitbucket",
+    logoSrc: bitbucketLogo
+  },
+  {
+    label: "VSCode",
+    logoSrc: vscodeLogo
+  },
+  {
+    label: "PyCharm",
+    logoSrc: pycharmLogo
+  }
+];
+
+export const cloudInfra = [
+  {
+    label: "Genesys",
+    logoSrc: genesysLogo
+  },
+  {
+    label: "Salesforce",
+    logoSrc: salesforceLogo
+  },
+];
+
+export const designCollab = [
+  {
+    label: "Figma",
+    logoSrc: figmaLogo
+  },
+  {
+    label: "Adobe Photoshop",
+    logoSrc: adobePhotoshopLogo
+  },
+  {
+    label: "Adobe Lightroom",
+    logoSrc: adobeLightroomLogo
+  }
+];
+
+export default { languages, frameworksLibraries, toolsPlatforms, cloudInfra, designCollab };
