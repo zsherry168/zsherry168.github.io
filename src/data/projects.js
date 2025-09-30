65caa88cf9daab63399ce045a2c60068346bf1e6
Sherry Zhang <zsherry168@gmail.com>
2025-09-29 21:29:57 -0400

    feat (projects): added old projects to the projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ import projectCover from "../assets/project_placeholder.jpg";
 
 const projects = [
   {
-    id: 2,
+    id: 4,
     title: "Java Project",
     date: "Work In Progress",
     desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit. 
@@ -11,7 +11,7 @@ const projects = [
     link: "/projects",
   },
   {
-    id: 1,
+    id: 3,
     title: "Personal Website",
     date: "Aug 2025 - Present",
     desc: `In id cursus mi pretium tellus duis convallis. 
@@ -19,6 +19,24 @@ const projects = [
     img: projectCover,
     link: "/projects",
   },
+  {
+    id: 2,
+    title: "Chit Chat",
+    date: "2023 (Refactored 2025)",
+    desc: `Quisque faucibus ex sapien vitae pellentesque sem placerat. 
+    In id cursus mi pretium tellus duis convallis.`,
+    img: projectCover,
+    link: "/projects"
+  },
+  {
+    id: 1,
+    title: "Nittany Reads",
+    date: "2023 (Refactored 2025)",
+    desc: `Tempus leo eu aenean sed diam urna tempor. 
+    Pulvinar vivamus fringilla lacus nec metus bibendum egestas.`,
+    img: projectCover,
+    link: "/projects"
+  }
 ];
 
 export default projects;
