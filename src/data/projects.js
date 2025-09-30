eaee6a3a99f5040782423167ab82b57c2f2336c9
Sherry Zhang <93848016+zsherry168@users.noreply.github.com>
2025-09-29 21:40:39 -0400

    feat (portfolio): add legacy projects and fix featured projects on home (#36)
    * feat (projects): added old projects to the projects page
    
    * fix (home): show only featured projects on home page

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,23 +2,45 @@ import projectCover from "../assets/project_placeholder.jpg";
 
 const projects = [
   {
-    id: 2,
+    id: 4,
     title: "Java Project",
     date: "Work In Progress",
     desc: `Lorem ipsum dolor sit amet consectetur adipiscing elit. 
     Quisque faucibus ex sapien vitae pellentesque sem placerat.`,
     img: projectCover,
     link: "/projects",
+    featured: false
   },
   {
-    id: 1,
+    id: 3,
     title: "Personal Website",
     date: "Aug 2025 - Present",
     desc: `In id cursus mi pretium tellus duis convallis. 
     Tempus leo eu aenean sed diam urna tempor.`,
     img: projectCover,
     link: "/projects",
+    featured: true
   },
+  {
+    id: 2,
+    title: "Chit Chat",
+    date: "2023 (Refactored 2025)",
+    desc: `Quisque faucibus ex sapien vitae pellentesque sem placerat. 
+    In id cursus mi pretium tellus duis convallis.`,
+    img: projectCover,
+    link: "/projects",
+    featured: true
+  },
+  {
+    id: 1,
+    title: "Nittany Reads",
+    date: "2023 (Refactored 2025)",
+    desc: `Tempus leo eu aenean sed diam urna tempor. 
+    Pulvinar vivamus fringilla lacus nec metus bibendum egestas.`,
+    img: projectCover,
+    link: "/projects",
+    featured: true
+  }
 ];
 
 export default projects;
