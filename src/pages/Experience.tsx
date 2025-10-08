819167b1c19b52180b0660851e11620b99e239ac
Sherry Zhang <93848016+zsherry168@users.noreply.github.com>
2025-10-07 20:53:45 -0400

    chore: readme and cleaning up whitespace/comments (#43)
    * docs (readme): updated readme
    
    * chore: cleanup whitespace and comments

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -7,7 +7,6 @@ export default function Experience() {
   return (
     <Container className="py-4" style={{ marginTop: "80px", marginBottom: "20px" }}>
       <h1 className="fw-bold display-5 mb-4">Experience</h1>
-
       <ExperienceSection title="Professional Experience" items={professional} />
       <ExperienceSection title="Academic Experience" items={academic} className="mt-5" />
       <ExperienceSection title="Leadership & Involvement" items={leadership} className="mt-5" />
