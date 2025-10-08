328ac3e36cfd79493be12fbb9ebda8491de621ae
Sherry Zhang <zsherry168@gmail.com>
2025-10-07 20:37:19 -0400

    refactor (routes): routing is in separate file from App

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,22 @@
+// src/routes/AppRoutes.tsx
+import { Routes, Route } from "react-router-dom";
+import Layout from "../layout/Layout.tsx";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Experience from "../pages/Experience";
+import Blog from "../pages/Blog";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="experience" element={<Experience />} />
+        <Route path="blog" element={<Blog />} />
+      </Route>
+    </Routes>
+  );
+}
