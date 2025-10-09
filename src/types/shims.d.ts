9311a0ed99c21604e884f742a617b536bfe059d5
Sherry Zhang <zsherry168@gmail.com>
2025-10-09 13:54:18 -0400

    chore (mdx): installed mdx for ts app

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,3 +1,10 @@
 declare module "*.png"  { const src: string; export default src }
 declare module "*.jpg"  { const src: string; export default src }
 declare module "*.jpeg" { const src: string; export default src }
+
+declare module "*.mdx" {
+  import { ComponentType } from "react";
+  const MDXComponent: ComponentType<any>;
+  export default MDXComponent;
+  export const frontmatter: any;
+}
